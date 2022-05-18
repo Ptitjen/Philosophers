4b42018fb912a3faeba89665006984624094a4f7
Jenny Eulliot <jeulliot@k0r1p7.42mulhouse.fr>
2022-05-18 10:30:06 +0200

    some changes

--- philo/philosophers.h
+++ philo/philosophers.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeulliot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jeulliot <jeulliot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 17:42:19 by jeulliot          #+#    #+#             */
-/*   Updated: 2022/04/26 17:42:21 by jeulliot         ###   ########.fr       */
+/*   Updated: 2022/05/17 15:29:43 by jeulliot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,21 @@
 # define PHILOSOPHERS_H
 # include <unistd.h>
 # include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <stdio.h>
 
 typedef struct s_philo_param {
-    int nb;
-    int ttd;
-    int tte;
-    int tts;
-    int npe;
+	int nb;
+	int ttd;
+	int tte;
+	int tts;
+	int npe;
 }   t_philo_param;
 
 int	    ft_atoi(const char *str);
 void    ft_arg_error(int argc, char **argv);
+void	ft_putnbr_fd(int n, int fd);
+void    ft_display_time(void);
 #endif
--- philo/utils.c
+++ philo/utils.c
@@ -3,11 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeulliot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jeulliot <jeulliot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 19:08:41 by jeulliot          #+#    #+#             */
-/*   Updated: 2022/04/26 19:08:43 by jeulliot         ###   ########.fr       */
+/*   Updated: 2022/05/17 15:35:39 by jeulliot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philosophers.h"
 
+void    ft_display_time(void)
+{
+	struct timeval current_time;
+
+	gettimeofday(&current_time, NULL);
+	ft_putnbr_fd(current_time.tv_usec, 1);   
+}
+
+//printf("\nTime seconds: %ld, time ms: %d\n", current_time.tv_sec, current_time.tv_usec); 
